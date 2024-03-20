dc0e70dce6e924ae36a75259816f094c222cc480
Liat Brochman <liatbrochman@gmail.com>
2024-03-20 13:36:39 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,28 @@ import compression from "compression";
 import { ConfigService } from "@nestjs/config";
 import session from "express-session";
 import MongoStore from "connect-mongo";
+import fs from "fs";
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const paths = {
+    key: __dirname.slice(0, -4) + "ssl\\cert.key",
+    cert: __dirname.slice(0, -4) + "ssl\\cert.crt"
+  };
+
+  if (!fs.existsSync(paths.key) || !fs.existsSync(paths.cert)) {
+    throw new Error("SSL certificates not found");
+  }
+
+  const httpsOptions = {
+    key: fs.readFileSync(paths.key),
+    cert: fs.readFileSync(paths.cert)
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+
   const configService = app.get(ConfigService);
 
   let cookie = {
@@ -17,7 +35,7 @@ async function bootstrap() {
     httpOnly: configService.get("HTTP_ONLY") == "true",
     secure: configService.get("SECURE") == "true",
     domain: configService.get("COOKIE_DOMAIN"),
-    path: '/',
+    path: "/"
   };
   console.log("cookie:");
   console.log(cookie);
